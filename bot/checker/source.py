9a6e1bff666f47898d0376363022f8b4fc64b028
Roma <mishenin_r@mail.ry>
2023-12-26 12:41:11 +0300

    transferred to main bot

--- bot/checker/source.py
+++ bot/checker/source.py
@@ -7,5 +7,5 @@ NAME = 'BotChecker'
 URL = 'https://statistics-api.wildberries.ru/api/v1/supplier/stocks'
 ROWS = ['supplierArticle', 'quantity', 'time']
 DATE_FROM = '2023-06-01'
-SHEET_ID = '1QfRcVy8pnxG3ZISe1x751akTh49FcMOu53FerNzXFQg'
-# SHEET_ID = '1rmXz1joBFlYDkPK4gY0BkbNaqAzhQ9F6Nle6xe-w4LE'
+# SHEET_ID = '1QfRcVy8pnxG3ZISe1x751akTh49FcMOu53FerNzXFQg'
+SHEET_ID = '1rmXz1joBFlYDkPK4gY0BkbNaqAzhQ9F6Nle6xe-w4LE'
--- bot/source.py
+++ bot/source.py
@@ -3,4 +3,4 @@ from threading import Thread
 
 main = '6833627182:AAFlKnO1BOsGn1P9eIqNlNNmpNPCz6R6D1M'
 test = '6811526530:AAHKNIrSMjvyfd-4u_zGPcpLVrYXTGBr4pI'
-bot = telebot.TeleBot(test)
+bot = telebot.TeleBot(main)
