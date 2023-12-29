a6c1a29a66a4be610f766d5461b09aaccc79f7bf
Roma <mishenin_r@mail.ry>
2023-12-29 04:49:27 +0300

    add keyboard

--- bot/source.py
+++ bot/source.py
@@ -3,7 +3,7 @@ from bot.report.main import *
 
 
 config, sections = ParseConfig('bot')
-bot = telebot.TeleBot(config[sections[1]]['Token'])
+bot = telebot.TeleBot(config[sections[0]]['Token'])
 TIME_CHECKER = '00:00'
 TIME_REPORT = '17:05:00'
 PATH_TO_DB = 'bot/chats.txt'
