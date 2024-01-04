b1cf3265a7669331e85d570725dba74948e224f8
Roma <mishenin_r@mail.ry>
2024-01-04 04:49:53 +0300

    solved double slash problem

--- bot/checker/source.py
+++ bot/checker/source.py
@@ -4,7 +4,7 @@ from common import *
 LONG_SLEEP = 90
 MAX_DIFF = 10
 MAX_LEN = 3500
-PATH = os.path.dirname(os.path.realpath(__file__)).rsplit('\\', 2)
+PATH = (os.path.dirname(os.path.realpath(__file__))).replace('\\', '/').split('/')
 TYPOLOGY = 'Test' if DEBUG_MODE else 'Main'
 URL = 'https://statistics-api.wildberries.ru/api/v1/supplier/stocks'
 ROWS = ['supplierArticle', 'quantity', 'time']
