753c297b95b056aa2b98467f50e61e064b4aab37
Roma <mishenin_r@mail.ry>
2024-01-13 04:49:48 +0300

    change data for the start to be less than year

--- analytics/source.py
+++ analytics/source.py
@@ -6,7 +6,7 @@ SHORT_SLEEP = 10
 NAME = (os.path.dirname(os.path.realpath(__file__))).replace('\\', '/').split('/')[-1]
 URL = 'https://api-seller.ozon.ru/v1/analytics/data'
 COLUMNS = {'id': None, 'name': None, 'revenue': 0, 'ordered_units': 1, 'returns': 2, 'delivered_units': 3, 'position_category': 4}
-START_OF_ALL = '2023-01-01'
+START_OF_ALL = (datetime.now() - timedelta(days=360)).strftime('%Y-%m-%d')
 SAMPLE = {
     'date_from': None,
     'date_to': None,
