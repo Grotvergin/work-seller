2a3f7c32bf7299a51dd50114d9be3fbfa618b76c
Roma <mishenin_r@mail.ry>
2024-04-04 15:01:15 +0300

    removed 2 periods from funnel

--- funnel/source.py
+++ funnel/source.py
@@ -6,8 +6,6 @@ PERIODS = {
     'DayBeforeYesterday': {'Start': (date.today() - timedelta(days=2)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=2)).strftime('%Y-%m-%d') + ' 23:59:59'},
     '3Days': {'Start': (date.today() - timedelta(days=3)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': datetime.now().strftime('%Y-%m-%d %H:%M:%S')},
     '6-3Days': {'Start': (date.today() - timedelta(days=6)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=3)).strftime('%Y-%m-%d') + ' 23:59:59'},
-    '3Months': {'Start': (date.today() - timedelta(days=91)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': datetime.now().strftime('%Y-%m-%d %H:%M:%S')},
-    '6-3Months': {'Start': (date.today() - timedelta(days=183)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=91)).strftime('%Y-%m-%d') + ' 23:59:59'}
 }
 URL = 'https://suppliers-api.wb.ru/content/v1/analytics/nm-report/detail'
 NAME = (os.path.dirname(os.path.realpath(__file__))).replace('\\', '/').split('/')[-1]
