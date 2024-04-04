a652061c70c6a8514d704c60757f91245adbca86
Roma <mishenin_r@mail.ry>
2024-04-04 15:14:57 +0300

    changed url in funnel and returned periods

--- funnel/source.py
+++ funnel/source.py
@@ -6,8 +6,10 @@ PERIODS = {
     'DayBeforeYesterday': {'Start': (date.today() - timedelta(days=2)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=2)).strftime('%Y-%m-%d') + ' 23:59:59'},
     '3Days': {'Start': (date.today() - timedelta(days=3)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': datetime.now().strftime('%Y-%m-%d %H:%M:%S')},
     '6-3Days': {'Start': (date.today() - timedelta(days=6)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=3)).strftime('%Y-%m-%d') + ' 23:59:59'},
+    '3Months': {'Start': (date.today() - timedelta(days=91)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': datetime.now().strftime('%Y-%m-%d %H:%M:%S')},
+    '6-3Months': {'Start': (date.today() - timedelta(days=183)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=91)).strftime('%Y-%m-%d') + ' 23:59:59'}
 }
-URL = 'https://suppliers-api.wb.ru/content/v1/analytics/nm-report/detail'
+URL = 'https://seller-analytics-api.wildberries.ru/api/v2/nm-report/detail'
 NAME = (os.path.dirname(os.path.realpath(__file__))).replace('\\', '/').split('/')[-1]
 LONG_SLEEP = 60
 SHORT_SLEEP = 20
