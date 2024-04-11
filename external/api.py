df33f1506d13d66eae4cb8379e1cfecddf9b8dc2
Roma <mishenin_r@mail.ry>
2024-04-11 22:15:43 +0300

    dsafgdb

--- external/api.py
+++ external/api.py
@@ -5,11 +5,16 @@ app = FastAPI()
 
 @app.get('/renew')
 def run_program():
+    Stamp('Request to renew accepted', 'i')
     if AddToDatabase('external', PATH_DB + 'active.txt', True):
+        Stamp('Check passed, starting renew', 's')
         thread = Thread(target=subprocess.run, args=(['python', '-m', 'external.main'],), kwargs={'check': False})
         thread.start()
+        Stamp('Thread was given', 'b')
         while thread.is_alive():
+            Stamp('Process is alive', 'i')
             time.sleep(1)
+            Stamp('Process is finished', 'b')
         RemoveFromDatabase('external', PATH_DB + 'active.txt')
         return {'message': 'Program executed successfully'}
     return {'message': 'Program is already running'}
