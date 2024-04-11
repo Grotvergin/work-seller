b9fb582e36bab9dc3f345906166527eac14de8d9
Roma <mishenin_r@mail.ry>
2024-04-11 22:11:08 +0300

    replace main

--- external/api.py
+++ external/api.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get('/renew')
 def run_program():
     if AddToDatabase('external', PATH_DB + 'active.txt', True):
-        thread = Thread(target=subprocess.run, args=(['python', '-m', 'external.api'],), kwargs={'check': False})
+        thread = Thread(target=subprocess.run, args=(['python', '-m', 'external.main'],), kwargs={'check': False})
         thread.start()
         while thread.is_alive():
             time.sleep(1)
