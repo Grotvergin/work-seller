bdfab40dc1b50266bee3dec1d9f5647fee8ab135
Roma <mishenin_r@mail.ry>
2024-05-22 20:55:59 +0300

    add week interval in funnel

--- funnel/source.py
+++ funnel/source.py
@@ -5,9 +5,10 @@ PERIODS = {
     'Yesterday': {'Start': (date.today() - timedelta(days=1)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=1)).strftime('%Y-%m-%d') + ' 23:59:59'},
     'DayBeforeYesterday': {'Start': (date.today() - timedelta(days=2)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=2)).strftime('%Y-%m-%d') + ' 23:59:59'},
     '3Days': {'Start': (date.today() - timedelta(days=3)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': datetime.now().strftime('%Y-%m-%d %H:%M:%S')},
-    '6-3Days': {'Start': (date.today() - timedelta(days=6)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=3)).strftime('%Y-%m-%d') + ' 23:59:59'},
+    '6-3Days': {'Start': (date.today() - timedelta(days=6)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=3)).strftime('%Y-%m-%d ') + ' 23:59:59'},
+    'Week': {'Start': (date.today() - timedelta(days=7)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': datetime.now().strftime('%Y-%m-%d %H:%M:%S')},
     '3Months': {'Start': (date.today() - timedelta(days=91)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': datetime.now().strftime('%Y-%m-%d %H:%M:%S')},
-    '6-3Months': {'Start': (date.today() - timedelta(days=183)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=91)).strftime('%Y-%m-%d') + ' 23:59:59'}
+    '6-3Months': {'Start': (date.today() - timedelta(days=183)).strftime('%Y-%m-%d') + ' 00:00:00', 'Finish': (date.today() - timedelta(days=91)).strftime('%Y-%m-%d') + ' 23:59:59'},
 }
 URL = 'https://seller-analytics-api.wildberries.ru/api/v2/nm-report/detail'
 NAME = (os.path.dirname(os.path.realpath(__file__))).replace('\\', '/').split('/')[-1]
