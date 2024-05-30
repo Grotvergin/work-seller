edd152388a0ed13ab58101cd3474a3cdbbf06f03
Roma <mishenin_r@mail.ry>
2024-05-30 17:09:23 +0300

    changed period in advert to 28 days

--- advert/source.py
+++ advert/source.py
@@ -6,7 +6,7 @@ URL_STAT = 'https://advert-api.wb.ru/adv/v2/fullstats'
 SLEEP = 60
 THR_DELTA = 15
 PORTION = 100
-BEGIN = '2023-01-01'
+BEGIN = (datetime.now() - timedelta(days=28)).strftime('%Y-%m-%d')
 NAME = (os.path.dirname(os.path.realpath(__file__))).replace('\\', '/').split('/')[-1]
 TYPES_AND_NAMES = {
     4: 'в каталоге',
