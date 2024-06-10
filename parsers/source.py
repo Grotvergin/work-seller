867127697c901081515098e1bb2debc6328e5a80
Roma <mishenin_r@mail.ry>
2024-06-10 14:04:24 +0300

    changed headers and url in parsers

--- parsers/source.py
+++ parsers/source.py
@@ -1,7 +1,7 @@
 from common import *
 
 
-URL = 'https://search.wb.ru/exactmatch/ru/male/v4/search'
+URL = 'https://search.wb.ru/exactmatch/ru/common/v5/search'
 COLUMNS = ['id', 'name', 'word', 'page', 'place',  'price', 'time']
 PREFIX = 'NoLog'
 NAME = (os.path.dirname(os.path.realpath(__file__))).replace('\\', '/').split('/')[-1]
@@ -19,9 +19,6 @@ PARAMS = {
     'sort': 'popular',
     'spp': '30',
     'suppressSpellcheck': 'false',
-    'uclusters': '1',
-    'uiv': '2',
-    'uv': 'pcurgKtTrZ8qY7FdL0-vCjAuqbE02ShGqcouqqbDHJkn2yz0FGGv2aIRLv-ixSwpJrEYviqGIogjCy1DL54xnK4zsXqhGKcfqoAh-TAOLxIsyieRLX8uMayjrlUrz7IwrKguC6kIrqAk06nDIACllCi-pBmrN6eipJ6w-DCjMPsy0i0dKtirlq6aLbWqc6s0JaUoWy4BJN6yaKwfnyiw_SdaHssxoyoRkOssN6pYqY0vxaw5LIitOLDtsQqroaywKCuozagEL6MtIiRlINyZkKWYlkQnm6iJJ-gnprK9qzouZK3kFjoqUycilfea3LC0LdarCx5lsAokz6-umAUnVQ',
 }
 
 HEADERS = {
@@ -29,7 +26,7 @@ HEADERS = {
     'Accept-Language': 'ru,en;q=0.9,cy;q=0.8',
     'Connection': 'keep-alive',
     'Origin': 'https://www.wildberries.ru',
-    'Referer': 'https://www.wildberries.ru/catalog/0/search.aspx?search=%D1%81%D0%BC%D0%B5%D1%81%D0%B8%D1%82%D0%B5%D0%BB%D1%8C',
+    'Referer': 'https://www.wildberries.ru/catalog/0/search.aspx',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'cross-site',
@@ -37,5 +34,5 @@ HEADERS = {
     'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "YaBrowser";v="23"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
-    'x-queryid': 'qid276998376170013136520240421113024',
+    'x-queryid': 'qid276998376170013136520240610105312',
 }
