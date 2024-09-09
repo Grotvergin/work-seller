d1fc936daa1aa30b5db6b612cb2bcf98cc142af9
Roma <mishenin_r@mail.ry>
2024-09-09 23:31:42 +0300

    change max attempts

--- common.py
+++ common.py
@@ -28,17 +28,17 @@ from colorama import Fore, Style, init
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-import chromedriver_autoinstaller
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import undetected_chromedriver
-from fastapi import FastAPI
-import uvicorn
-from urllib.parse import quote
+# import chromedriver_autoinstaller
+# from selenium import webdriver
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.common.exceptions import WebDriverException
+# import undetected_chromedriver
+# from fastapi import FastAPI
+# import uvicorn
+# from urllib.parse import quote
 
 
 init()
--- pricepec/source.py
+++ pricepec/source.py
@@ -5,7 +5,7 @@ COLUMNS = ['id', 'name', 'price', 'time']
 NAME = (os.path.dirname(os.path.realpath(__file__))).replace('\\', '/').split('/')[-1]
 SHORT_SLEEP = 6
 LONG_SLEEP = 45
-MAX_ATTEMPTS = 5
+MAX_ATTEMPTS = 20
 
 PARAMS = {
     'appType': '1',
